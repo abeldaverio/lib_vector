53c268ce82ea0bfbd36edf89b1d0c7b83f1bb6d0
Abel DAVERIO <abel.daverio@epitech.eu>
2024-12-29 19:37:06 +0100

    fix: merge

--- include/vector.h
+++ include/vector.h
@@ -29,11 +29,6 @@ void *push_index_vector(void *vector, size_t index, void *data);
 void popfront_vector(void *vector);
 void pop_index_vector(void *vector, size_t index);
 void *concat_vector(int nb_of_vectors, ...);
-<<<<<<< Updated upstream
-
-// internal used functions
-vector_t *_get_vector_struct(void *vector);
-=======
 void quick_sort_vector(void *vector,
     int (*function)(void *first, void *second));
 ssize_t index_of_vector(void *vector, void *to_find);
@@ -43,6 +38,5 @@ vector_t *_get_vector_struct(void *vector);
 bool _sort(vector_t *vect, int low, int high,
     int (*function)(void *first, void *second));
 bool _compare_bytes(char *first, char *second, size_t n);
->>>>>>> Stashed changes
 
 static const size_t INIT_SIZE = 5;
